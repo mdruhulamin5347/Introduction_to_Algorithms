4ef2362d42c47547c11035fbb849a3bbf9a51de4
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2025-02-06 01:33:28 +0600

    implement adjacency list

---
+++ Graph Representaion/adjacency_list.cpp
@@ -0,0 +1,28 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,e;
+    cin >> n >> e;
+
+    vector<int> adj_list[n];
+
+    memset(adj_list,-1,sizeof(adj_list));
+
+    for(int i=0; i<e; i++){
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    for(int i=0; i<n; i++){
+        for(int val : adj_list[i]){
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
