271bbb0e37c9117357ab91f477a47828736f9919
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2025-02-06 14:54:13 +0600

    shortest path print

---
+++ BFS/shortest_path_print.cpp
@@ -0,0 +1,58 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[1005];
+bool vis[1005]={false};
+int level[1005]={-1};
+int parent[1005]={-1};
+
+void bfs(int src){
+    queue<int>q;
+    q.push(src);
+    vis[src]=true;
+    level[src]=0;
+    while(!q.empty()){
+        int par = q.front();
+        q.pop();
+
+
+        for(int child : adj_list[par]){
+            if(vis[child] == false){
+                q.push(child);
+                vis[child]=true;
+                level[child]=level[par]+1;
+                parent[child] = par;
+            }
+        }
+    }
+}
+
+int main(){
+    int n,e;
+    cin >> n >> e;
+    while(e--){
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    int src,dst;
+    cin >> src >> dst;
+    bfs(src);
+
+    vector<int>v;
+    int node = dst;
+    while(node != -1){
+        v.push_back(node);
+        node = parent[node];
+    }
+    reverse(v.begin(),v.end());
+    for(int val : v){
+        cout << val << " ";
+    }
+    return 0;
+}
