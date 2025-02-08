96ee8e6a9c594bb1a05995b959945edb74eb1640
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2025-02-09 01:47:31 +0600

    third problem solve: count node

--- BFS DFS Practice Problem/problem_number_3.cpp
+++ BFS DFS Practice Problem/problem_number_3.cpp
@@ -10,14 +10,17 @@ using namespace std;
 vector<int> adj_list[1005];
 
 int main(){
+
     int n,e;
     cin >> n >> e;
+    
     while(e--){
         int a,b;
         cin >> a >> b;
         adj_list[a].push_back(b);
         adj_list[b].push_back(a);
     }
+
     int x;
     cin >> x;
 
