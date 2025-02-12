e7fdad7506cde5ee12c41decb1270ad2bfd68d6d
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2025-02-12 23:46:51 +0600

    Dijkstra Algorithm solve by more optimize approch

---
+++ Dijkstra Algorithm/dijkstra_optimized.cpp
@@ -0,0 +1,46 @@
+
+// diakstra algorithm solve by more optimize approch
+
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<pair<int,int>> adj_list[1005];
+vector<int>path(1005,INT_MAX);
+
+void dijkstra(int src){
+    priority_queue<pair<int,int>> pq;
+    pq.push({src,0});
+    path[src]=0;
+    while(!pq.empty()){
+        pair<int,int> par = pq.top();
+        pq.pop();
+        int par_dis = par.first;
+        int par_node = par.second;
+
+        for(auto child : adj_list[par_node]){
+            int child_node = child.first;
+            int child_dis = child.second;
+            if(par_dis + child_dis < path[child_node]){
+                path[child_node] = par_dis + child_dis;
+                pq.push({path[child_node],child_node});
+            }
+        }
+    }
+}
+
+int main(){
+    int n,e;
+    cin >> n >> e;
+    while(e--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        adj_list[a].push_back({b,c});
+        adj_list[b].push_back({a,c});
+    }
+
+    dijkstra(0);
+
+    for(int i=0; i<n; i++)
+        cout << path[i] << " ";
+    return 0;
+}
