e1569de27f0c0f47896f47684a456f83618d8203
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-02-25 22:15:11 +0600

    implementation shortest path find using bellman ford algorithom

---
+++ Bellman Ford/bellman_ford.cpp
@@ -0,0 +1,46 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Edge{
+    public:
+    int a,b,c;
+    Edge(int a,int b,int c){
+        this->a = a;
+        this->b = b;
+        this->c =c;
+    }
+};
+
+int main(){
+    vector<int>dis(1005,INT_MAX);
+    int n,e;
+    cin >> n >> e;
+    vector<Edge> edge_list;    
+
+    while(e--){
+        int a,b,c;
+        
+        cin >> a >> b >> c;
+        edge_list.push_back(Edge(a,b,c));        
+    }
+    dis[0]=0;
+    for(int i=0; i<n-1; i++){
+        for(auto ad : edge_list){
+            int a,b,c;
+            a = ad.a;
+            b = ad.b;
+            c = ad.c;
+            if(dis[a] != INT_MAX && dis[a] + c < dis[b]){
+                dis[b] = dis[a] + c;
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        cout << i << "->" << dis[i] << endl;
+    }
+    
+    return 0;
+}
